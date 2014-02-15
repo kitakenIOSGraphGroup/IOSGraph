d8968225c5992c8188faee3c52a341b130012491
jineq <e115765@iu.u-ryukyu.ac.jp>
2014-02-15 13:00:33 +0900

    Created project files

---
+++ IOSGraph/IOSGraph/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IOSGraph
+//
+//  Created by vilayouth vongsomxai on 2014/02/15.
+//  Copyright (c) 2014年 vilayouth vongsomxai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IOSGraph/IOSGraph/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IOSGraph
+//
+//  Created by vilayouth vongsomxai on 2014/02/15.
+//  Copyright (c) 2014年 vilayouth vongsomxai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
